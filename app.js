c440cac9cc20b00e1158ef03e1955350cf0a186d
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-08 14:45:40 +0100

    ADD use.static

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("./", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Ciao mamma!");
 });
+
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`server is listenting on port ${port}`);
