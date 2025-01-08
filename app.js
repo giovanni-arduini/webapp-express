d0f842bad3c2b56f340db78831057555b8be0968
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-08 14:49:29 +0100

    ADD notFound middleware

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const notFound = require("./middlewares/notFound");
 
 app.get("/", (req, res) => {
   res.send("Ciao mamma!");
 });
 
 app.use(express.static("public"));
+
+app.use(notFound);
 
 app.listen(port, () => {
   console.log(`server is listenting on port ${port}`);
