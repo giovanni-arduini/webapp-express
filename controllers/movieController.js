916b627d11c76b3e56f202dd1958df88091d6929
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-08 15:34:00 +0100

    FIX show function in controller

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -15,12 +15,20 @@ function index(req, res) {
 
 function show(req, res) {
   const id = parseInt(req.params.id);
-  const sql = `SELECT * FROM movies WHERE id = ${id}`;
+  let sql = `SELECT * FROM movies WHERE id = ${id}`;
 
-  connection.query(sql, (err, results) => {
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ message: err.message });
+    if (results.length === 0) return res.status(404);
+
     const movie = results[0];
 
-    res.json({ movie });
+    sql = `SELECT * FROM reviews WHERE movie_id = ${id}`;
+    connection.query(sql, [id], (err, results) => {
+      if (err) return res.status(500).json({ message: err.message });
+      movie.reviews = results;
+      res.json({ movie });
+    });
   });
 }
 
