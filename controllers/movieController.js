a482edccd40f309cbbc9216af66a9e549e382d62
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-12 17:33:10 +0100

    FIX title filter search query

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,11 +1,16 @@
 const connection = require("../data/db");
 
 function index(req, res) {
-  const sql = `SELECT * FROM movies`;
+  let sql = `SELECT movies.*, AVG(vote) AS avg_vote FROM movies JOIN reviews ON movies.id = reviews.movie_id`;
+
+  console.log(req.query);
 
   if (req.query.title) {
-    sql += `WHERE title LIKE ${req.query.title}`;
+    console.log(req.query.title);
+    sql += ` WHERE title LIKE '%${req.query.title}%'`;
   }
+
+  sql += ` GROUP BY movies.id`;
 
   connection.query(sql, (err, movies) => {
     console.log(err);
@@ -21,9 +26,16 @@ function index(req, res) {
   });
 }
 
+// ------------------
+
 function show(req, res) {
   const id = parseInt(req.params.id);
-  let sql = `SELECT * FROM movies WHERE id = ${id}`;
+  let sql = `SELECT movies.*, AVG(vote) AS avg_vote
+ FROM movies 
+ JOIN reviews 
+ ON movies.id = reviews.movie_id 
+ WHERE movies.id=${id}
+ GROUP BY movies.id`;
 
   connection.query(sql, [id], (err, results) => {
     if (err) return res.status(500).json({ message: err.message });
@@ -31,11 +43,14 @@ function show(req, res) {
 
     const movie = results[0];
 
+    console.log(results);
+
     sql = `SELECT * FROM reviews WHERE movie_id = ${id}`;
     connection.query(sql, [id], (err, results) => {
       if (err) return res.status(500).json({ message: err.message });
       movie.reviews = results;
       res.json({ movie, success: true });
+      console.log(movie);
     });
   });
 }
